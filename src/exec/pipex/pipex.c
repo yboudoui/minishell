d4c90d161bf6f303ae8d7a0200cd8b5c688bb5f8
Karim DHRIF <kdhrif@e2r4p7.clusters.42paris.fr>
2023-02-24 15:26:40 +0100

    Working on infile resovler

--- src/exec/pipex/pipex.c
+++ src/exec/pipex/pipex.c
@@ -6,13 +6,23 @@
 /*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 15:22:32 by kdhrif            #+#    #+#             */
-/*   Updated: 2023/02/22 15:41:01 by kdhrif           ###   ########.fr       */
+/*   Updated: 2023/02/24 15:26:28 by kdhrif           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/minishell.h"
 
-/* int pipex(t_list *env, t_prompt *prompt) */
-/* { */
+int pipex(t_list env, t_prompt prompt)
+{
+	t_list cmd;
 
-/* } */
+	cmd = prompt->commande;
+	//First checks the redir_in
+}
+
+int infile(t_list redir_in)
+{
+
+
+
+}
