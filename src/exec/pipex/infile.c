a2f4f2a9c9451090908e75e5acda42a7af1cf8dd
Karim DHRIF <kdhrif@e2r10p15.clusters.42paris.fr>
2023-02-27 18:59:24 +0100

    Feat:Infile works

--- src/exec/pipex/infile.c
+++ src/exec/pipex/infile.c
@@ -6,11 +6,12 @@
 /*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 17:21:19 by kdhrif            #+#    #+#             */
-/*   Updated: 2023/02/27 17:37:22 by kdhrif           ###   ########.fr       */
+/*   Updated: 2023/02/27 18:57:59 by kdhrif           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/minishell.h"
+#include <stdlib.h>
 #include <unistd.h>
 
 int infile(t_list redir_in)
@@ -19,24 +20,16 @@ int infile(t_list redir_in)
 	t_token token;
 
 	if (redir_in == NULL)
-	{
-		printf("redir_in is NULL\n");
-		return (STDIN_FILENO);
-	}
-	printf("HERE\n");
-	token = (t_token)redir_in->content;
-	printf("redir_in = %s\n", token->input);
-	printf("redir_in = %p\n", token->input);
-	if (redir_in->content == NULL)
-		printf("it works\n");
-	exit(1);
-	if (redir_in == NULL)
 		return (STDIN_FILENO);
 	fd = -1;
 	while (redir_in)
 	{
-		fd = f_open((char *)redir_in->content, O_RDONLY, 0);
+		token = redir_in->content;
+		fd = f_open(token->input, O_RDONLY, 0);
+		printf("fd = %d\n", fd);
 		if (fd == -1 && redir_in->next == NULL)
+			return (-1);
+		if (fd == -1 && redir_in->next != NULL)
 			return (-1);
 		if (fd != -1 && redir_in->next != NULL)
 			close_fd(&fd);
