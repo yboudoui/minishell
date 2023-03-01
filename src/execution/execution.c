e5d487e24af1bc546c6e96827c4b75ec75240330
Karim DHRIF <kdhrif@e2r11p16.clusters.42paris.fr>
2023-03-01 14:38:25 +0100

    merged leak/fork bug

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,7 +6,11 @@
 /*   By: yboudoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 15:15:32 by yboudoui          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2023/03/01 12:41:51 by yboudoui         ###   ########.fr       */
+=======
+/*   Updated: 2023/03/01 11:56:35 by kdhrif           ###   ########.fr       */
+>>>>>>> 5fb2087 (x)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +32,11 @@ int	execution(t_list env, t_prompt prompt)
 	if (heredoc(prompt))
 		return (EXIT_FAILURE);
 	expand_all_command(env, prompt);
+<<<<<<< HEAD
 	if (pipex(env, prompt))
 		return (EXIT_FAILURE);
+=======
+	pipex(env, prompt);
+>>>>>>> 5fb2087 (x)
 	return (EXIT_SUCCESS);
 }
