2a669257c7a83a777fb7b4c2fce79842585e1e3d
Karim DHRIF <kdhrif@e2r11p21.clusters.42paris.fr>
2023-03-17 19:38:14 +0100

    fix builtins plus cd without args

--- src/execution/builtin/cd.c
+++ src/execution/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: yboudoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 07:52:52 by yboudoui          #+#    #+#             */
-/*   Updated: 2023/03/17 19:23:26 by kdhrif           ###   ########.fr       */
+/*   Updated: 2023/03/17 19:32:01 by kdhrif           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,14 @@ int	check_error(char **argv)
 	return (0);
 }
 
+int check_source(char *cdvar)
+{
+	chdir(cdvar);
+	free(cdvar);
+	env_list_insert_new("PWD", print_working_directory("chdir"));
+	return (0);
+}
+
 int	builtin_cd(char *argv[])
 {
 	char	*old_pwd;
@@ -37,15 +45,17 @@ int	builtin_cd(char *argv[])
 	argv += 1;
 	if (*argv == NULL || ft_strncmp(*argv, "~", ft_strlen(*argv)) == 0)
 	{
-		argv = env_get_value("HOME", 0 ,0);
+		cdvar = env_get_value("HOME", 0 ,0);
 		reset = env_get_value("PWD", 0, 0);
 		env_list_insert_new("OLDPWD", ft_strdup(reset));	
 		free(reset);
-	}
-	if (*argv == NULL)
-	{
-		generic_err("cd", "HOME not set\n", 2);
-		return(1);
+		if (cdvar == NULL)
+		{
+			generic_err("cd", "HOME not set\n", 2);
+			return(1);
+		}
+		check_source(cdvar);
+		return (0);
 	}
 	else
 		old_pwd = print_working_directory("chdir");
